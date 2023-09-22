07eeb46310ac87a948844d3e234dfe24914c8bb8
J. Garcia <janikgar@gmail.com>
2023-09-21 23:15:12 -0700

    version bumps

---
+++ packages/app/cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  fixturesFolder: false,
+  retries: 3,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:3001',
+  },
+})
