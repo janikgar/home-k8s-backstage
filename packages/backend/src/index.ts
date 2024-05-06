15fbd20521e66fb6152423fa925cb75e1b3c1f9d
J. Garcia <janikgar@gmail.com>
2024-05-05 21:48:44 -0700

    re-add github actions

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -82,6 +82,7 @@ backend.add(import('@backstage/plugin-catalog-backend/alpha'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'));
 
 backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
