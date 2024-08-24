dbb980adcb655a6074ae32f3ffbb2fc05a92892c
J. Garcia <janikgar@pm.me>
2024-08-24 16:33:06 -0700

    version bump

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -87,14 +87,21 @@ backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'));
 
 backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
 
+// See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
+backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
+
 // permission plugin
 backend.add(import('@backstage/plugin-permission-backend/alpha'));
+// See https://backstage.io/docs/permissions/getting-started for how to create your own permission policy
 backend.add(
   import('@backstage/plugin-permission-backend-module-allow-all-policy'),
 );
 
 // search plugin
-backend.add(import('@backstage/plugin-search-backend/alpha'));
+backend.add(import('@backstage/plugin-search-backend/alpha'));// search engine
+// See https://backstage.io/docs/features/search/search-engines
+backend.add(import('@backstage/plugin-search-backend-module-pg/alpha'));
+// search collators
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
