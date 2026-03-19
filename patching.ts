c4692fa292423f21ec355ad8d5c877bafe47ede9
J. Garcia <janikgar@pm.me>
2026-03-18 23:12:40 -0700

    first cut at autopatching

---
+++ patching.ts
@@ -0,0 +1,154 @@
+import {execSync} from "node:child_process"
+import * as fs from "node:fs/promises"
+import * as semver from "semver"
+
+const DOCKER_BINARY = "podman"
+const TRIVY_COMMAND = `${DOCKER_BINARY} run -v trivy:/cache -v $PWD:/repo aquasec/trivy repository --cache-dir /cache`
+
+class ExecSyncReturns<T> {
+    pid!: number;
+    output!: Array<T | null>;
+    stdout!: T;
+    stderr!: T;
+    status!: number | null;
+    signal!: NodeJS.Signals | null;
+    error?: Error;
+
+    constructor(stdout: T) {
+        this.stdout = stdout;
+    }
+}
+
+class Package {
+    currentVersion: string;
+    patchCandidates: Array<string>;
+    closestCandidate: string;
+    closestCandidateDiff: semver.ReleaseType;
+
+    constructor(currentVersion: string, patchCandidates: Array<string>){
+        this.currentVersion = currentVersion;
+        let patchSet = new Set<string>();
+        for (let patchCandidate of patchCandidates) {
+            patchSet.add(patchCandidate);
+        }
+        this.patchCandidates = new Array(...patchSet).sort();
+        this.closestCandidate = this.patchCandidates[0];
+        this.closestCandidateDiff = semver.diff(this.currentVersion, this.closestCandidate)!;
+    }
+
+    /**
+     * merge
+     */
+    public merge(currentVersion: string, patchCandidates: Array<string>) {
+        this.currentVersion = currentVersion;
+        let patchSet = new Set<string>(this.patchCandidates);
+        patchCandidates.map((patchCandidate) => {patchSet.add(patchCandidate)});
+        this.patchCandidates = semver.sort(new Array(...patchSet).sort());
+    }
+}
+
+// Run a command line command and return the output
+function runCommand(command: string): string {
+    try {
+        const output = execSync(command, { encoding: "utf-8" });
+        let outputAsSyncOutput: ExecSyncReturns<string> = JSON.parse(output);
+        return outputAsSyncOutput.stdout;
+    } catch (errorOutput: any) {
+        let errorOutputAsSyncOutput: ExecSyncReturns<string> = errorOutput;
+        return errorOutputAsSyncOutput.stdout;
+    }
+}
+
+async function readTrivyResults(fileName: string): Promise<object> {
+    let resultFile = {};
+    let fileHandle: fs.FileHandle;
+
+    return new Promise((resolve, reject) => {
+        fs.open(fileName).then((handle) => {
+            fileHandle = handle;
+        }).then(() => {
+            return fileHandle.readFile({encoding: "utf-8"})
+        }).then((fileContents) => {
+            resultFile = JSON.parse(fileContents)
+            resolve(resultFile)
+        }).catch((error) => {
+            reject(error)
+        }).finally(() => {
+            fileHandle.close()
+        })
+    })
+}
+
+function parseTrivyResults(results: any): Map<string, Package> {
+    let importantResults = new Map<string, Package>();
+
+    for (let result of results['Results'][0]['Vulnerabilities']) {
+        let currentVersion = result['InstalledVersion'];
+        let fixedVersions = result['FixedVersion'].split(",") as Array<string>;
+
+        fixedVersions = fixedVersions.map((value) => value.trim());
+
+        if (importantResults.has(result['PkgName'])) {
+            importantResults.get(result['PkgName'])?.merge(currentVersion, fixedVersions);
+        } else {
+            let newPackage = new Package(currentVersion, fixedVersions);
+            importantResults.set(result['PkgName'], newPackage);
+        }
+    }
+    return importantResults
+}
+
+function prettyPrintResults(parsedResults: Map<string, Package>) {
+    console.log(parsedResults);
+    for (let [pkgName, pkgList] of parsedResults) {
+        let curVer = pkgList.currentVersion;
+        let bestCandidate = pkgList.closestCandidate;
+        let semverDiff = pkgList.closestCandidateDiff;
+        console.log(`${pkgName}: ${curVer} -> ${bestCandidate}: (${semverDiff})`);
+    }
+}
+
+function patchStuff(parsedResults: Map<string, Package>) {
+    for (let [pkgName, contents] of parsedResults) {
+        attemptUpdate(pkgName, contents.closestCandidate)
+    }
+}
+
+function attemptUpdate(pkgName: string, version: string) {
+    let fileHandle: fs.FileHandle;
+    let pkgJson: any;
+
+    fs.open("./package.json", fs.constants.O_RDWR).then((handle) => {
+        fileHandle = handle;
+    }).then(() => {
+        return fileHandle.readFile({encoding: "utf-8"});
+    }).then((contents) => {
+        pkgJson = JSON.parse(contents);
+        pkgJson['resolutions'][pkgName] = `~${version}`;
+
+        fileHandle.writeFile(JSON.stringify(pkgJson));
+    }).catch((reason) => {
+        console.log(reason);
+    }).finally(() => {
+        fileHandle.close();
+    })
+
+    // runCommand(`yarn install`)
+}
+
+runCommand(`${TRIVY_COMMAND} --download-db-only`);
+
+const scanOutput = runCommand(`${TRIVY_COMMAND} --skip-db-update -f json -o /repo/vulns.json --ignore-unfixed --scanners vuln . `);
+
+console.log(scanOutput);
+
+console.log("reading result");
+let results = await readTrivyResults("./vulns.json");
+
+console.log("parsing result");
+let parsedResults = parseTrivyResults(results);
+
+console.log("pretty printing result");
+prettyPrintResults(parsedResults);
+
+patchStuff(parsedResults);
